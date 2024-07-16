6c67574a944174d3a9510a8236b0e3c3d986149e
hsj._.06 <gtw030488@gmail.com>
2024-07-16 14:42:33 +0900

    init :: Project

--- Project.swift
+++ Project.swift
@@ -1,18 +1,18 @@
 import ProjectDescription
 
-let projectName = ""
-let organizationName = ""
+let projectName = "WaIrony"
+let organizationName = "twinkleSun"
 
 let project = Project(
     name: projectName,
     organizationName: organizationName,
     targets: [
-        Target(
+        .init(
             name: "\(projectName)",
             platform: .iOS,
             product: .app,
             bundleId: "\(organizationName).\(projectName)",
-            deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
+            deploymentTarget: .iOS(targetVersion: "16.0", devices: .iphone),
             infoPlist: .file(path: Path("Support/Info.plist")),
             sources: ["Source/**"],
             resources: ["Resource/**"]
